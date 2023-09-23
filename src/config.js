5428d36eff3dbf1887b8c81436617a35ecf2cf32
j84176168 <juana.soria@huawei.com>
2023-09-23 15:04:23 -0600

    Solve env config variable error

--- src/config.js
+++ src/config.js
@@ -2,4 +2,9 @@ import { config } from 'dotenv';
 
 config();
 
-export const { PORT = 3000, DB_USER, DB_PASSWORD, DB_DATABASE, DB_HOST, DB_PORT } = (process.env.PORT)
+export const PORT = process.env.PORT;
+export const DB_PORT = process.env.DB_PORT;
+export const DB_USER = process.env.DB_USER
+export const DB_PASSWORD = process.env.DB_PASSWORD
+export const DB_DATABASE = process.env.DB_DATABASE
+export const DB_HOST = process.env.DB_HOST
